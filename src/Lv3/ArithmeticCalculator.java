8d378dedb80b609827599f16b20eaa53e8362294
roog23 <roog2320@gmail.com>
2025-04-21 09:01:12 +0900

    jdk-17 & 객체명 수정

--- src/Lv3/ArithmeticCalculator.java
+++ src/Lv3/ArithmeticCalculator.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class ArithmeticCalculator<T> {
     private T num1,num2;
     // 결과 값을 저장하는 캡슐화된 객체입니다.
-    private final List<Double> ResultList = new ArrayList<>();
+    private final List<Double> resultList = new ArrayList<>();
 
     public void setNumber1(T num) {     // num1에 값을 저장하기 위한 세터입니다.
         this.num1= num;
@@ -28,13 +28,13 @@ public class ArithmeticCalculator<T> {
     // 계산 기능을 수행하고 결과값을 저장하는 메소드입니다.
     public double calculator(char sign) {
         double result = OperatorType.calculator((double)num1,(double)num2,sign);
-        ResultList.add(result);
+        resultList.add(result);
         return result;
     }
 
     // 입력 받은 값보다 큰 결과 값을 출력해주는 메소드입니다.
     public void checkResult(double checkNum) {
-        List<Double> ret = ResultList.stream()
+        List<Double> ret = resultList.stream()
                                      .filter(num -> num > checkNum)
                                      .collect(Collectors.toList());
         if (ret.isEmpty()) {    // 입력 받은 값보다 큰 결과 값이 없는 경우 실행됩니다.
